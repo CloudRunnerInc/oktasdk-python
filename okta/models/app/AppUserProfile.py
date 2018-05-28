15bcaea943a31fc120e0e4d5071157c9e6670763
Vitalii Kulanov <vkulanov@waverleysoftware.com>
2018-05-28 10:10:16 +0300

    Allow custom attributes for AppUser (Application User) model

--- okta/models/app/AppUserProfile.py
+++ okta/models/app/AppUserProfile.py
@@ -1,12 +1,2 @@
 class AppUserProfile:
-
-    types = {
-        'username': str,
-        'password': str
-    }
-
-    def __init__(self):
-
-        self.username = None  # str
-
-        self.password = None  # str
+    pass
